c9a9dbdfb1c8709ef7abc67968ea7edee0f7c5b5
westfechtel <bernhard.westfechtel@uni-bayreuth.de>
2024-10-12 14:01:56 +0200

    Update SpielfeldLeserImpl.java

--- sudoku/src/de/bt/bw/sudoku/SpielfeldLeserImpl.java
+++ sudoku/src/de/bt/bw/sudoku/SpielfeldLeserImpl.java
@@ -3,7 +3,9 @@
  */
 package de.bt.bw.sudoku;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * 
@@ -15,8 +17,38 @@ public class SpielfeldLeserImpl implements SpielfeldLeser {
 	@Override
 	public Spielfeld lies(String name)
 			throws FileNotFoundException, NumberFormatException, FalscheZeilenanzahl, FalscheZeilenlaenge, FalscheZahl {
-		// TODO Auto-generated method stub
-		return null;
+		Scanner scanner = new Scanner(new File(name));
+		Spielfeld spielfeld = new SpielfeldImpl();
+		int zeilenIndex = 0;
+		while (scanner.hasNextLine()) {
+			String zeile = scanner.nextLine();
+			if (!zeile.trim().isEmpty()) { // Leerzeilen überspringen
+				zeilenIndex++;
+				if (zeilenIndex > 9) {
+					scanner.close();
+					throw new FalscheZeilenanzahl(zeilenIndex);
+				}					
+				String[] zahlen = zeile.split("\\s+"); // Zahlen durch nichtleere Folgen von Leerzeichen getrennt
+				int zeilenLaenge = zahlen.length;
+				if (zeilenLaenge != 9) {
+					scanner.close();
+					throw new FalscheZeilenlaenge(zeilenIndex, zeilenLaenge);
+				}					
+				for (int spaltenindex = 1; spaltenindex <= 9; spaltenindex++) {
+					int wert = Integer.parseInt(zahlen[spaltenindex-1]); // Achtung: Arrayindizes beginnen bei 0
+					if (0 <= wert && wert <= 9) 
+						spielfeld.setze(zeilenIndex, spaltenindex, wert);
+					else {
+						scanner.close();
+						throw new FalscheZahl(wert, 0, 9);
+					}						
+				}	
+			}			
+		}
+		scanner.close();
+		if (zeilenIndex < 9)
+			throw new FalscheZeilenanzahl(zeilenIndex);		
+		return spielfeld;
 	}
 
 }
