f193f137c14ec9caf670e232049511915322f799
westfechtel <bernhard.westfechtel@uni-bayreuth.de>
2024-11-15 11:32:33 +0100

    Erste Experimente zur Sicht
    Mit Buttons, Combo-Boxen, Flow- und GridLayout experimentiert

--- sudoku/README.txt
+++ sudoku/README.txt
@@ -1,4 +1,8 @@
-Beim Testen wird u.U. ein Fehler gemeldet. Dann unter Project das Kommando BuildAutomatically
+Init startet das interaktive Werkzeug (fensterbasiert).
+LoeserKonsole startet das automatische Werkzeuge (Konsole).
+
+Beim Testen oder Ausführen wird u.U. ein Fehler gemeldet. 
+Dann unter Project das Kommando BuildAutomatically
 deaktivieren, anschließend Clean und Build Project aktivieren. 
 
 Alle Lösungen zu Rätseln aus Rätselbüchern sind eindeutig. Deshalb wird beim Testen
--- sudoku/src/de/bt/bw/sudoku/Sicht.java
+++ sudoku/src/de/bt/bw/sudoku/Sicht.java
@@ -3,9 +3,13 @@
  */
 package de.bt.bw.sudoku;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /**
@@ -15,20 +19,68 @@ public class Sicht extends JPanel implements Beobachter, ActionListener {
     private Kontrolle kontrolle;
     private Modell modell;
     
+    private static final String[] zahlen = {" ", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    // private static final Integer[] zahlen = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    
+    // JComboBox<Integer> beispiel;
+    
+    JButton laden, speichern, fertig;
+    
+    JComboBox[][] zellen;
+    
     public Sicht(Kontrolle kontrolle, Modell modell) {
+    	
+    	// MVC-Verbindungen initialisieren
         this.kontrolle = kontrolle;
         this.modell = modell;
-        
-        // Initialisierung der Sicht
-        
-        // Registrierung als Beobachter beim Modell
         modell.registriere(this);
+        
+        // Standard-Layout: Flow Layout
+    	
+    	// Buttons für Kommandos
+    	laden = new JButton("Laden");
+    	laden.addActionListener(this);
+    	this.add(laden);
+    	speichern = new JButton("Speichern");
+    	speichern.addActionListener(this);
+    	this.add(speichern);
+    	fertig = new JButton("Fertig");
+    	fertig.addActionListener(this);
+    	this.add(fertig);
+        
+        // Spielfeld mit Combo-Boxen darstellen
+    	// this.setLayout(new GridLayout(9,9));
+        zellen = new JComboBox[9][9];
+        for (int zeilenNr = 0; zeilenNr < 9; zeilenNr++) 
+        	for (int spaltenNr = 0; spaltenNr < 9; spaltenNr++) {
+        		JComboBox aktuelleZelle = new JComboBox(zahlen);
+        		zellen[zeilenNr][spaltenNr] = aktuelleZelle;
+        		aktuelleZelle.addActionListener(this);
+        		this.add(aktuelleZelle);        		
+        	}
+        
+        // Rahmen erzeugen und anzeigen
+        JFrame frame = new JFrame("Sudoku");
+        frame.getContentPane().add(this);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,800);
+        frame.setVisible(true);        
     }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
+		// Bisher nur Testausgaben auf der Konsole
+		if (e.getSource() instanceof JComboBox) {
+			// Combo-Box
+			JComboBox zelle = (JComboBox)e.getSource();
+			String selektion = (String)zelle.getSelectedItem();
+			System.out.println("Selektion: " + selektion);
+		} else {
+			// Buttons
+			JButton button = (JButton)e.getSource();
+			String kommando = e.getActionCommand();
+			System.out.println("Kommando: " + kommando);
+		}
 	}
 
 	@Override
